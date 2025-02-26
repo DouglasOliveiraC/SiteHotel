52ac966cc8491d8acd80af915ad87dd031920e57
Douglas Oliveira <douoliver@gmail.com>
2025-02-26 12:49:26 -0300

    - Corrigindo Vite para permitir acesso no Railway

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,13 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url)), // Aponta para a pasta 'src'
         },
     },
+    server: {
+        host: '0.0.0.0', // Permite acesso externo no Railway
+        port: 3000, // Mantém a porta consistente com a configuração do Docker e Railway
+        strictPort: true, // Garante que o Vite não tente outras portas
+    },
     build: {
-        outDir: 'dist',  // Garante que os arquivos de produção vão para a pasta correta
+        outDir: 'dist', // Garante que os arquivos de produção vão para a pasta correta
         emptyOutDir: true, // Limpa a pasta antes do build
     }
 });
