d0932d3ffe287ec4ef489a4c1726bad3a175ef56
Douglas Oliveira <douoliver@gmail.com>
2025-03-07 21:31:09 -0300

    - Configurações para endpoint

--- api/server.js
+++ api/server.js
@@ -11,6 +11,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 app.use(express.static('dist'));
 
 const PORT = process.env.PORT || 3000;
